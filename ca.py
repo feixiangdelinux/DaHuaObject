08f6aac77e4996568fcc41e79b05494cab78c646
Linux <linux2005@163.com>
2023-02-05 22:42:19 +0800

    3

---
+++ ca.py
@@ -0,0 +1,93 @@
+import time
+
+import pyautogui
+
+if __name__ == '__main__':
+    print('开始')
+    start_time = time.time()
+    # pyautogui.leftClick(x=451 + 120, y=163 - 20, duration=0.2)
+    # time.sleep(1)
+
+    # pyautogui.moveTo(x=300, y=200)
+    # time.sleep(1)
+    # pyautogui.dragTo(x=300 + 700, y=200, duration=1, button='left')
+    # time.sleep(1)
+
+    # for i in range(90):
+    #     pyautogui.leftClick(x=1120, y=770, duration=0.12)
+    # pyautogui.rightClick(x=275, y=600, duration=0.12)
+
+    # pyautogui.moveTo(x=100, y=10)
+    # time.sleep(1)
+    # pyautogui.dragTo(x=100 + 1430, y=10, duration=1, button='left')
+    # time.sleep(1)
+    # pyautogui.moveTo(x=100, y=10)
+    # time.sleep(1)
+    # pyautogui.dragTo(x=100, y=10 + 1050, duration=1, button='left')
+    # time.sleep(1)
+    # pyautogui.moveTo(x=100, y=10)
+    # time.sleep(1)
+    # pyautogui.dragTo(x=100 + 1430, y=10 + 1050, duration=1, button='left')
+    # time.sleep(1)
+
+    # for j in range(100):
+    #     print(j)
+    #     for i in range(90):
+    #         pyautogui.leftClick(x=1120, y=770)
+    #         pyautogui.leftClick(x=1120 + 1430, y=770)
+    #         pyautogui.leftClick(x=1120, y=770 + 1050)
+    #         pyautogui.leftClick(x=1120 + 1430, y=770 + 1050)
+    #     pyautogui.rightClick(x=275, y=600, duration=0.04)
+    #     pyautogui.rightClick(x=275 + 1430, y=600, duration=0.04)
+    #     pyautogui.rightClick(x=275, y=600 + 1050, duration=0.04)
+    #     pyautogui.rightClick(x=275 + 1430, y=600 + 1050, duration=0.04)
+
+    for i in range(10000):
+        print(i)
+        pyautogui.leftClick(x=950, y=710)
+        pyautogui.leftClick(x=950 + 1430, y=710)
+        pyautogui.leftClick(x=950, y=710 + 1050)
+        pyautogui.leftClick(x=950 + 1430, y=710 + 1050)
+        time.sleep(4.2)
+
+    # pyautogui.leftClick(x=(670 * 1.5), y=(680 * 1.5), duration=0.2)
+    # for z in range(3):
+    #     pyautogui.leftClick(x=(465 * 1.5), y=((384 + (35 * (z + 1))) * 1.5), duration=0.2)
+    #     for x in range(6):
+    #         for y in range(4):
+    #             pyautogui.leftClick(x=((175 + (51 * x)) * 1.5), y=((393 + (51 * y)) * 1.5), duration=0.1)
+    #             pyautogui.leftClick(x=(70 * 1.5), y=(315 * 1.5), duration=0.1)
+    #             pyautogui.leftClick(x=(430 * 1.5), y=(240 * 1.5), duration=0.12)
+    # pyautogui.leftClick(x=(670 * 1.5), y=(680 * 1.5), duration=0.2)
+    #
+    # pyautogui.leftClick(x=(670 * 1.5) + 1430, y=(680 * 1.5), duration=0.2)
+    # for z in range(3):
+    #     pyautogui.leftClick(x=(465 * 1.5) + 1430, y=((384 + (35 * (z + 1))) * 1.5), duration=0.2)
+    #     for x in range(6):
+    #         for y in range(4):
+    #             pyautogui.leftClick(x=((175 + (51 * x)) * 1.5) + 1430, y=((393 + (51 * y)) * 1.5), duration=0.1)
+    #             pyautogui.leftClick(x=(70 * 1.5) + 1430, y=(315 * 1.5), duration=0.1)
+    #             pyautogui.leftClick(x=(430 * 1.5) + 1430, y=(240 * 1.5), duration=0.12)
+    # pyautogui.leftClick(x=(670 * 1.5) + 1430, y=(680 * 1.5), duration=0.2)
+    #
+    # pyautogui.leftClick(x=(670 * 1.5), y=(680 * 1.5) + 1050, duration=0.2)
+    # for z in range(3):
+    #     pyautogui.leftClick(x=(465 * 1.5), y=((384 + (35 * (z + 1))) * 1.5) + 1050, duration=0.2)
+    #     for x in range(6):
+    #         for y in range(4):
+    #             pyautogui.leftClick(x=((175 + (51 * x)) * 1.5), y=((393 + (51 * y)) * 1.5) + 1050, duration=0.1)
+    #             pyautogui.leftClick(x=(70 * 1.5), y=(315 * 1.5) + 1050, duration=0.1)
+    #             pyautogui.leftClick(x=(430 * 1.5), y=(240 * 1.5) + 1050, duration=0.12)
+    # pyautogui.leftClick(x=(670 * 1.5), y=(680 * 1.5) + 1050, duration=0.2)
+    #
+    # pyautogui.leftClick(x=(670 * 1.5) + 1430, y=(680 * 1.5) + 1050, duration=0.2)
+    # for z in range(3):
+    #     pyautogui.leftClick(x=(465 * 1.5) + 1430, y=((384 + (35 * (z + 1))) * 1.5) + 1050, duration=0.2)
+    #     for x in range(6):
+    #         for y in range(4):
+    #             pyautogui.leftClick(x=((175 + (51 * x)) * 1.5) + 1430, y=((393 + (51 * y)) * 1.5) + 1050, duration=0.1)
+    #             pyautogui.leftClick(x=(70 * 1.5) + 1430, y=(315 * 1.5) + 1050, duration=0.1)
+    #             pyautogui.leftClick(x=(430 * 1.5) + 1430, y=(240 * 1.5) + 1050, duration=0.12)
+    # pyautogui.leftClick(x=(670 * 1.5) + 1430, y=(680 * 1.5) + 1050, duration=0.2)
+    end_time = time.time()
+    print(end_time - start_time)
